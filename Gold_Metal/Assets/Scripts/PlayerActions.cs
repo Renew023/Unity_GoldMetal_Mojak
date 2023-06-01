ff08309b14e51ff8e3387c123853c78f82aa5b91
Renew023 <ya1539@naver.com>
2023-06-01 22:54:25 +0900

    골드메탈 B21 끝
    나머지는 애초에 남은게 없었어서 쉬웠다.

--- Gold_Metal/Assets/Scripts/PlayerActions.cs
+++ Gold_Metal/Assets/Scripts/PlayerActions.cs
@@ -9,6 +9,8 @@ public class PlayerActions : MonoBehaviour
     float h;
     float v;
     bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
 
     Rigidbody2D rd;
 
@@ -34,7 +36,6 @@ public class PlayerActions : MonoBehaviour
         bool hUp = Input.GetButtonUp("Horizontal");
         bool vUp = Input.GetButtonUp("Vertical");
         
-        Debug.Log(v);
         //이동기
         if(hDown)
             isHorizonMove = true;
@@ -54,11 +55,31 @@ public class PlayerActions : MonoBehaviour
         else
             anim.SetBool("isChange", false);
 
-
+        if(vDown && v == 1)
+            dirVec = Vector3.up;
+        else if (vDown && v == -1)
+            dirVec = Vector3.down;
+        else if (hDown && h == -1)
+            dirVec = Vector3.left;
+        else if (hDown && h == 1)
+            dirVec = Vector3.right;
+        
+        if(Input.GetButtonDown("Jump") && scanObject != null)
+            Debug.Log(scanObject.name);
     }
 
     void FixedUpdate(){
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
         rd.velocity = moveVec * Speed;
+
+        //Ray
+        Debug.DrawRay(rd.position, dirVec * 0.7f, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rd.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if(rayHit.collider != null) {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+            scanObject = null;
     }
 }
